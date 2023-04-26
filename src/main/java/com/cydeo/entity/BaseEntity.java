305783281c6a482a6823a93382315f8fa4ce85ff
olegdj16 <92351568+olegdj16@users.noreply.github.com>
2023-04-26 18:24:15 -0500

    commit

---
+++ src/main/java/com/cydeo/entity/BaseEntity.java
@@ -0,0 +1,16 @@
+package com.cydeo.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@MappedSuperclass
+@Getter
+@Setter
+public class BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+}
---
+++ src/main/java/com/cydeo/entity/Category.java
@@ -0,0 +1,22 @@
+package com.cydeo.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class Category extends BaseEntity {
+
+    private String name;
+
+
+}
